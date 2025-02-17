56fff1b7bab7453a83578b3795bb7db1f9cde277
Adimgr1 <adimgr51@gmail.com>
2025-02-17 09:15:56 +0530

    add movie componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+import Movie from './Movie'
 
 function App() {
 
   return (
+   <BrowserRouter>
     <>
+    <Routes>
+      <Route path="/movies" element={<Movie/>}/>
+    </Routes>
      
     </>
+   
+   </BrowserRouter>
   )
 }
 
--- src/Movie.jsx
+++ src/Movie.jsx
@@ -1,7 +1,7 @@
 export default function Movie(){
      return(
           <>
-          
+          <h1> Movies</h1>
           </>
      )
 }
